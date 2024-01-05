5de54d82fb4b142fa31adf2019ca547e6b8fa9d7
Maddie <maddieeckstrom@gmail.com>
2024-01-04 16:09:17 -1000

    made about me section responsive, added example image and text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,10 @@
+import aboutmeimage from '../assets/images/exampleaboutme.jpg';
+
 export default function AboutMe() {
     return (
-        <section>
-            <p>Hello world</p>
-            <img src="../images/exampleaboutme.jpg"/>
+        <section className="sections">
+            <p>Aloha! My name is Madison Eckstrom. I am a web developer specializing in front-end development. I am located on the island of O'ahu, Hawai'i.</p>
+            <img className="aboutmeImage" src={aboutmeimage}/>
         </section>
     )
 }
