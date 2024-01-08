fd1546f3c3fdcf2d12fd757287f6a7d5ce482e71
Maddie <maddieeckstrom@gmail.com>
2024-01-08 11:34:33 -1000

    finished responsiveness and styling of aboutme page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,13 @@
+import backgroundimage from '../assets/images/background2.jpg';
 import aboutmeimage from '../assets/images/aboutme1.jpg';
-import aboutmeavatar from '../assets/images/avatar.png';
 
 export default function AboutMe() {
     return (
         <section className="sections">
-            <div>
-                <p className="aboutmeP"> Aloha! My name is Madison Eckstrom. I am a web developer specializing in front-end development located on the island of O'ahu, Hawai'i. My skills include HTML, CSS, JavaScript, API, React, and more. Outside of work you'll find me looking for good movies to watch, enjoying some sunshine, or cooking a gratifying meal. I have a positive and passionate work ethic and hope to continually impact others through my leadership and communication. I look forward to connecting with you!</p>
-                <img className="aboutmeAvatar" src={aboutmeavatar}/>
+            <div className="aboutmeContainer">
+                <img className="backgroundImage" src={backgroundimage}/>
+                <p className="aboutmeP"> Hi there, my name is Madison Eckstrom. I am a web developer specializing in front-end development located on the island of O'ahu, Hawai'i. My skills include HTML, CSS, JavaScript, API, React, and more. Outside of work you'll find me looking for good movies to watch, enjoying some sunshine, or cooking a gratifying meal. I have a positive and passionate work ethic and hope to continually impact others through my leadership and communication. I look forward to connecting with you, <br/> <br/>
+                Aloha!</p>
             </div>
             <img className="aboutmeImage" src={aboutmeimage}/>
         </section>
