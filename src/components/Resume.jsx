b8b8b819294d128c248d99891b183a04314cc969
Maddie <maddieeckstrom@gmail.com>
2024-01-08 15:45:41 -1000

    resume section completed and downloadable resume pdf available

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,24 @@
+import avatarImage from '../assets/images/avatar.png';
+
 export default function Resume() {
+
+    const onButtonClick = () => {
+        const pdfUrl = "https://docs.google.com/document/d/1jtWdCDDMOe7cwxrb-BXLSD0Y6tiw6A-AcAplADdhq30/export?format=pdf";
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.download = "Madisonresume.pdf"; // specify the filename
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
-        <p>
-            Hi
-        </p>
+        <section className="sectionsP">
+            <div className="resumeDiv">
+                <h2>download my resume here:</h2>
+                <button onClick={onButtonClick}>Download PDF</button>
+            </div>
+        <img className="avatarImage" src={avatarImage}></img>
+        </section>
     )
 }
