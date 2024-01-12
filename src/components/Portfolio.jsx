f68f41b515f758120e0c27619086c3490bbfad57
Maddie <maddieeckstrom@gmail.com>
2024-01-12 13:22:25 -1000

    adjust portfolio.jsx file to include one component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,64 +5,62 @@ import moviemiximage from '../assets/images/moviemixPic.png';
 import portfolioimage from '../assets/images/firstportfolioPic.png';
 import calendarimage from '../assets/images/calendarPic.png';
 
+const projects = [
+    {
+        title: "culinary connect",
+        image: culinaryimage,
+        github: "https://github.com/quar-tech/mvc-recipe-sharing",
+        site: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/"
+    },
+    {
+        title: "weather forecast",
+        image: weatherimage,
+        github: "https://github.com/quar-tech/mvc-recipe-sharing",
+        site: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/"
+    },
+    {
+        title: "api quiz",
+        image: quizimage,
+        github: "https://github.com/quar-tech/mvc-recipe-sharing",
+        site: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/"
+    },
+    {
+        title: "movie mix & sip",
+        image: moviemiximage,
+        github: "https://github.com/quar-tech/mvc-recipe-sharing",
+        site: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/"
+    },
+    {
+        title: "my first portfolio",
+        image: portfolioimage,
+        github: "https://github.com/quar-tech/mvc-recipe-sharing",
+        site: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/"
+    },
+    {
+        title: "calendar library",
+        image: calendarimage,
+        github: "https://github.com/quar-tech/mvc-recipe-sharing",
+        site: "https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/"
+    },
+
+]
+
 
 
 export default function Portfolio() {
     return (
         <section className="portfolio">
-            <div className="portfolioRow">
-                <div className="portfolioDiv">
-                    <h2>culinary connect</h2>
-                    <img className="portfolioCards" src={culinaryimage} alt="culinary connect"/>
-                    <div className="portfolioLinks">
-                        <a href="https://github.com/quar-tech/mvc-recipe-sharing">GitHub</a>
-                        <a href="https://mvc-culinary-connect-e00ad0e3e10d.herokuapp.com/">Live Site</a>
-                    </div>
-                </div>
-                <div className="portfolioDiv">
-                        <h2>weather forecast</h2>
-                        <img className="portfolioCards" src={weatherimage}/>
+            <div className="portfolioGrid">
+                {projects.map((project) => (
+                    <div className="portfolioDiv" key={project.title}>
+                        <h2>{project.title}</h2>
+                        <img className="portfolioCards" src={project.image} alt="culinary connect"/>
                         <div className="portfolioLinks">
-                            <a href="https://github.com/maddieeckstrom/weather-forecast-dashboard">GitHub</a>
-                            <a href="https://maddieeckstrom.github.io/weather-forecast-dashboard/">Live Site</a>
+                            <a href={project.github}>GitHub</a>
+                            <a href={project.site}>Live Site</a>
                         </div>
-                </div>
-            </div>
-            <div className="portfolioRow">
-                <div className="portfolioDiv">
-                    <h2>api quiz</h2>
-                    <img className="portfolioCards" src={quizimage}/>
-                    <div className="portfolioLinks">
-                        <a href="https://github.com/maddieeckstrom/API-timed-coding-quiz">GitHub</a>
-                        <a href="https://maddieeckstrom.github.io/API-timed-coding-quiz/">Live Site</a>
                     </div>
-                </div>
-                <div className="portfolioDiv">
-                    <h2>movie mix & sip</h2>
-                    <img className="portfolioCards" src={moviemiximage}/>
-                    <div className="portfolioLinks">
-                        <a href="https://github.com/ucb-allstars-project-1/movie-cocktail-combo">GitHub</a>
-                        <a href="https://ucb-allstars-project-1.github.io/movie-cocktail-combo/">Live Site</a>
-                    </div>
-                </div>
-            </div>
-            <div className="portfolioRow">
-                <div className="portfolioDiv">
-                    <h2>my first portfolio</h2>
-                    <img className="portfolioCards" src={portfolioimage}/>
-                    <div className="portfolioLinks">
-                        <a href="https://github.com/maddieeckstrom/Madison_super_portfolio">GitHub</a>
-                        <a href="https://maddieeckstrom.github.io/Madison_super_portfolio/">Live Site</a>
-                    </div>
-                </div>
-                <div className="portfolioDiv">
-                    <h2>calendar library</h2>
-                    <img className="portfolioCards" src={calendarimage}/>
-                    <div className="portfolioLinks">
-                        <a href="https://github.com/maddieeckstrom/Calendar-library-application">GitHub</a>
-                        <a href="https://maddieeckstrom.github.io/Calendar-library-application/">Live Site</a>
-                    </div>
-                </div>
+                ))}
             </div>
         </section>
     )
