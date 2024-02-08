e1cfe948297a98f801e5c378dc4acae2b116b872
Maddie <maddieeckstrom@gmail.com>
2024-02-07 15:39:32 -1000

    small gramatical change

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
         <section className="aboutmeSection">
             <div className="aboutmeContainer">
                 <img className="backgroundImage" src={backgroundimage}/>
-                <p className="aboutmeP"> My name is Madison Eckstrom, I am a web developer specializing in front-end development located on the island of O'ahu, Hawai'i. My skills include HTML, CSS, JavaScript, API, React, and more. Outside of work you'll find me looking for good movies to watch, enjoying some sunshine, or cooking a gratifying meal. I have a positive and passionate work ethic and hope to continually impact others through my leadership and communication. I look forward to connecting with you, <br/> <br/>
+                <p className="aboutmeP"> My name is Madison Eckstrom; I am a web developer specializing in front-end development located on the island of O'ahu, Hawai'i. My skills include HTML, CSS, JavaScript, API, React, and more. Outside of work you'll find me looking for good movies to watch, enjoying some sunshine, or cooking a gratifying meal. I have a positive and passionate work ethic and hope to continually impact others through my leadership and communication. I look forward to connecting with you, <br/> <br/>
                 Aloha!</p>
                 <img className="aboutmeImage" src={aboutmeimage}/>
             </div>
