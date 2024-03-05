60917ba99712e760da0dd596640cccbcf5997fe2
Maddie <maddieeckstrom@gmail.com>
2024-03-05 12:39:28 -1000

    edited linkedin link in footer and changed wording

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ export default function Footer() {
         <footer>
             <ul className="footer">
                 <li>
-                    <a href="https://github.com/maddieeckstrom">check my GitHub here</a>
+                    <a href="https://github.com/maddieeckstrom">My GitHub</a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/madison-eckstrom-56a725149/">check my LinkedIn here</a>
+                    <a href="https://www.linkedin.com/in/madison-eckstrom/">My LinkedIn</a>
                 </li>
             </ul>
         </footer>
