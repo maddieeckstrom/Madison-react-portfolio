861d2d2567693126dcb82880e5da916fcd96c3da
Maddie <maddieeckstrom@gmail.com>
2024-04-11 09:45:50 -1000

    edited about me paragraph and responsive styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -19,7 +19,9 @@ export default function AboutMe() {
                         <h1>Hello,</h1>
                         <h1>I'm Madison!</h1>
                     </div>
-                    <p>I am a web developer specializing in front-end development located on the island of O'ahu, Hawai'i. My skills include HTML, CSS, JavaScript, API, React, and more. I have a positive and passionate work ethic and hope to continually impact others through my leadership and communication. I look forward to connecting with you,</p>
+                    <p>I am a Front End Developer who was trained in full stack web development. I am skilled in HTML, CSS,
+JavaScript, REST API, React, and much more. I have an interest in design and I am looking for work as a
+Front End Developer or Engineer. I have a positive and passionate work ethic and hope to continually impact others through my leadership and communication. I look forward to connecting with you,</p>
                     <h3>Aloha!</h3>
                 </div>
                 <div className="myImgBox">
